a681e0cd4ec8cc803b748d4bf20aac4e2d7aaddf
Hazli Johar <coding@hazlijohar.com>
2025-08-14 16:59:30 +0800

    fix(app): remove landing footer from app shell to avoid footer showing in dashboard

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -4,7 +4,6 @@ import './index.css'
 import { RouterProvider, createRouter } from '@tanstack/react-router'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ClerkProvider } from '@clerk/clerk-react'
-import { SocialFooter } from './sections/SocialFooter'
 import { ThemeProvider } from './providers/ThemeProvider'
 import { routeTree } from './routeTree.gen'
 
@@ -38,7 +37,6 @@ createRoot(document.getElementById('root')!).render(
             <div className="flex-1">
               <RouterProvider router={router} />
             </div>
-            <SocialFooter />
           </div>
         </QueryClientProvider>
       </ThemeProvider>
